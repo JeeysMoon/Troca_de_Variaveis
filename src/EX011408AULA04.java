8a2fecdc5ca13923d1ed7ae1d391d3b8402c8151
Jennifer Sousa <jennifergaldinodesousa@gmail.com>
2022-01-28 20:38:35 -0300

    2801

---
+++ src/EX011408AULA04.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EX011408AULA04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Crie um programa que receba duas informações do mesmo tipo e mostre-as. Agora troque o valor das variáveis e mostre-as novamente.
+        
+       String nome = "Jennifer";
+        int idade = 25;
+             
+       
+        
+        System.out.println("Seu nome é " + nome + " e sua idade é: " + idade);
+        
+        
+        nome = "Joao";
+        idade = 15;
+        
+        System.out.println("Seu nome é " + nome + " e sua idade é: " + idade);
+    }
+    
+}
